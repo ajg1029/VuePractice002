ce1004b4e60d2abebbf8959019d88ac47e46d71a
Deon <urban.seonbi.deon@gmail.com>
2023-04-27 15:26:13 +0900

    axios test 1, 페이지(컴포넌트) 내부에서 호출

---
+++ 03_create_app_manually/hello-vuex/src/views/AxiosTest1.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <div>AxiosTest1.vue</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+
+export default {
+  name: 'AxiosTest1',
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  created() {
+    axios.defaults.baseURL = 'https://dev-orderservice.baedalgeek.kr'
+    axios({
+      url: '/api/v1/orders/',
+      method: 'get',
+      params: {
+        campusCode: '3A',
+        targetDate: '2023-04-01',
+        targetEndDate: '2023-04-30'
+      }
+    }).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+}
+</script>
